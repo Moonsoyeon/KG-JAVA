48f955a131bc004d1985f4b88de8c34510f6eadd
SoYeon, Moon <ansthdus2000@naver.com>
2021-01-08 12:03:27 +0900

    0107

---
+++ 0108/src/Main.java
@@ -0,0 +1,17 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		int num;
+		num = 5; //대입연산자
+		System.out.println("num = " + num);
+		
+		num = 10;
+		System.out.println("num = " + num);
+		
+		//num = num + 10;
+		System.out.println("num + 10 = " + (num + 10));
+		
+	}
+
+}

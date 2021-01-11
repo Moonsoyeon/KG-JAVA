15274d1e0fdc49dff25d40333893f899b4bd88da
SoYeon, Moon <ansthdus2000@naver.com>
2021-01-11 23:59:49 +0900

    0111

---
+++ 0111/src/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		boolean boolVar;
+		
+		boolVar = false;
+		System.out.println(boolVar);
+		
+		boolVar = true;
+		System.out.println(boolVar);
+
+	}
+
+}

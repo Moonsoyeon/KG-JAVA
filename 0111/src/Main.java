f9b6d67782c95c7e1cb155124fd5cf4399d359fe
SoYeon, Moon <ansthdus2000@naver.com>
2021-01-12 00:49:12 +0900

    0111

--- 0111/src/Main.java
+++ 0111/src/Main.java
@@ -2,14 +2,73 @@
 public class Main {
 
 	public static void main(String[] args) {
-		boolean boolVar;
+//		boolean boolVar;
+//		
+//		boolVar = false;
+//		System.out.println(boolVar);
+//		
+//		boolVar = true;
+//		System.out.println(boolVar);
+//		
+//		boolVar = 10>5;
+//		System.out.println(boolVar);
 		
-		boolVar = false;
-		System.out.println(boolVar);
+//		System.out.println(10);
+//		
+//		int fNum = 10, sNum;
+//		
+//		sNum = fNum;
+//		
+//		System.out.println(fNum);
+//		System.out.println(sNum);
+//		
+//		int tNum=10, fthNum=3;
+//		
+//		System.out.println(tNum=10);
+//		
+//		byte b = 127;
+//		System.out.println(b);
+//		
+//		b++;
+//		System.out.println(b);
+//		
+//		short s = 32767;
+//		System.out.println(s);
+//		
+//		long l = 0x7fffffffffffffffL;
+//		System.out.println(l);
+//		
+//		l = l+1;
+//		System.out.println(l);
+//	
+//		System.out.println(123.456);
+//		
+//		float f = 123.456F;
+//		double d = 234.567;
+//		
+//		System.out.println("f=" + f);
+//		System.out.println("d=" + d);
 		
-		boolVar = true;
-		System.out.println(boolVar);
-
+//		char ch = 'A';
+//		char code = '\u0041';
+//		
+//		System.out.println("ch=" + ch);
+//		System.out.println("code=" + code);
+//
+//		ch = '가';
+//		System.out.println("ch=" + ch);
+//		
+//		ch = '1' + '1';
+//		System.out.println("ch=" + ch);
+		
+		String name = "김강사";
+		System.out.println(name);
+		
+		String str = "재미있는 자바";
+		System.out.println(str);
+		
+		System.out.println(name+"와 함께하는 "+str);
+		
 	}
 
 }

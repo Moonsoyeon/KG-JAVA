2383455f925425b1f43f9efc174e395ff0225a38
SoYeon, Moon <ansthdus2000@naver.com>
2021-01-12 09:46:29 +0900

    0111

--- 0111/src/Main.java
+++ 0111/src/Main.java
@@ -60,15 +60,48 @@ public class Main {
 //		
 //		ch = '1' + '1';
 //		System.out.println("ch=" + ch);
+//		
+//		String name = "김강사";
+//		System.out.println(name);
+//		
+//		String str = "재미있는 자바";
+//		System.out.println(str);
+//		
+//		System.out.println(name+"와 함께하는 "+ str);
+//		
+//		final double pi = 3.141592;
+//		double area;
+//		int radian = 5;
+//		
+//		String name = "김강사";
+//		
+//		area = radian * radian * pi;
+//		
+//		System.out.println("반지름 : " + radian);
+//		System.out.println("원의 넓이 : " + area);
+//		
+//		int a = 5;
+//		int b = 3;
+//		
+//		System.out.println(a*b/2);
+//		System.out.println(a*b/2.0);
+//		System.out.println((double)a*(float)b/2);
+		//float와 double은 연산 불가능
+		//더 큰 값인 double로 변환
 		
-		String name = "김강사";
-		System.out.println(name);
+		byte b = 10;
+		int i = b;
 		
-		String str = "재미있는 자바";
-		System.out.println(str);
+		System.out.println("b = " + b);
+		System.out.println("i = " + i);
 		
-		System.out.println(name+"와 함께하는 "+str);
+		i = 1024;
+		b = (byte)i;
+		//i는 4byte b는 1byte
 		
+		System.out.println("b = " + b);
+		// => 1byte만 남고 짤려버림 (1024 = 10 0000 0000)
+		System.out.println("i = " + i);
 	}
 
 }

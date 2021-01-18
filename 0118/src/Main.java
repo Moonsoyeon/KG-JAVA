31a1bfcc119e852d7cbdf9aba7ff4168886716a5
SoYeon, Moon <ansthdus2000@naver.com>
2021-01-19 01:54:04 +0900

    0118

--- 0118/src/Main.java
+++ 0118/src/Main.java
@@ -1,7 +1,8 @@
 import java.io.IOException;
+import java.io.IOException;
 import java.util.Scanner;
 
-public class T {
+public class Main {
 
 	public static void main(String[] args) throws IOException {
 //		Scanner sc = new Scanner(System.in);
@@ -23,6 +24,8 @@ public class T {
 //			System.out.println("정상적이지 않은 값입니다.");
 //		}
 //		System.out.println("프로그램 종료");
+		
+//		------------------------------------------
 		
 //		Scanner sc = new Scanner(System.in);
 //		
@@ -52,6 +55,8 @@ public class T {
 //		
 //		System.out.println("점수 : " +score+ "점");
 //		System.out.println("등급 : " +grade+ "등급");
+
+//		------------------------------------------
 		
 //		사용자로 부터 여행 가기 원하는 달을 입력을 받으세요
 //		12-2 : 겨울 
@@ -64,93 +69,108 @@ public class T {
 //		여름 : 제트스키
 //		가을 : 경비행기
 //		겨울 : 평생 시즌권
-//		
+		
 //		Scanner sc = new Scanner(System.in);
-//		
-//		String season = "";
-//		String coupon = "";
-//		
-//		System.out.print("여행가고 싶은 달 : ");
+		
+//		System.out.print("여행 가고 싶은 달 : ");
 //		int month = sc.nextInt();
-//		
-//		switch(month) {
-//		case 3 : 
-//		case 4 :
-//		case 5 :
+		
+//		String season = "";
+//		String gift = "";
+		
+//		switch (month) {
+//		case 3:
+//		case 4:
+//		case 5:
 //			season = "봄";
-//			coupon = "자전거";
+//			gift = "자전거";
 //			break;
-//		
-//		case 6 :
-//		case 7 :
-//		case 8 :
+			
+//		case 6:
+//		case 7:
+//		case 8:
 //			season = "여름";
-//			coupon = "제트스키";
+//			gift = "제트스키";
 //			break;
-//		
-//		case 9 :
-//		case 10 :
-//		case 11 :
+			
+//		case 9:
+//		case 10:
+//		case 11:
 //			season = "가을";
-//			coupon = "경비행기";
+//			gift = "경비행기";
 //			break;
-//		
-//		case 12 :
-//		case 1 :
-//		case 2 :
+			
+//		case 12:
+//		case 1:
+//		case 2:
 //			season = "겨울";
-//			coupon = "평생 시즌권";
+//			gift = "평생 시즌권";
 //			break;
-//		default :
+			
+//		default:
 //			season = "행운의 계절";
-//			coupon = "행운";
+//			gift = "행운";
 //		}
-//		
-//		System.out.print(season+ "에 여행 가시면 ");
-//		System.out.println(coupon+ "을(를) 사은품으로 증정합니다.");
+		
+//		System.out.println(season + "에 여행을 가시면 " + gift + "을(를) 선물로 드립니다.");
+		
+//		------------------------------------------
+		
 //		Scanner sc = new  Scanner(System.in);
-//		
+		
 //		System.out.print("문자 입력 : ");
 //		String label = sc.next();
 //		int ch = System.in.read();
-//		
+		
 //		char label = (char)ch;
 //		switch(label) {
 //		case 'A' :
 //			System.out.println("A가 입력 되었습니다.");
 //			break;
+
 //		case 'B' :
 //			System.out.println("B가 입력 되었습니다.");
 //			break;
+
 //		case 'C' :
 //			System.out.println("C가 입력 되었습니다.");
 //			break;
+
 //		default :
 //			System.out.println("ABC가 아닌 문자 입력 ");
+		
 //		}
 		
+//		------------------------------------------
+		
 //		Scanner sc = new Scanner(System.in);
-//		
+		
 //		String kor = "";
 //		
 //		System.out.print("영어 단어 입력 : ");
 //		String strEng = sc.nextLine();
 //		
-//		switch(strEng) {
-//		case "hello" :
+//		switch (strEng) {
+//		case "hello":
 //			kor = "안녕하세요";
 //			break;
-//		case "good day mate" :
+//
+//		case "good day":
 //			kor = "좋은 날이야";
 //			break;
-//		case "thank you" :
-//			kor = "감사합니다.";
-//		default :
-//			kor = "잘못된 문장 입니다.";		
+//		
+//		case "thank you":
+//			kor = "감사합니다";
+//			break;
+//
+//		default:
+//			kor = "잘못된 문장";
 //		}
 //		
-//		System.out.print(strEng+ " 를 번역하면");
-//		System.out.println("[" +kor+ "]입니다.");
+//		System.out.print(strEng + "를 번역하면 ");
+//		System.out.println(" [" + kor + "]입니다.");
+		
+//		------------------------------------------
 		
 //		수강 신청 프로그램
 //		과목 : 
@@ -166,18 +186,53 @@ public class T {
 //		강의실 : 
 //		강사:
 		
+//		Scanner sc = new Scanner(System.in);
+		
+//		System.out.print("수강 신청 과목 : ");
+//		String subject = sc.nextLine();
+//		String room = "";
+//		String teacher = "";
+		
+//		switch (subject) {
+//		case "java":
+//			room = "301호";
+//			teacher = "자강사";
+//			break;
+			
+//		case "c언어":
+//			room = "302호";
+//			teacher = "씨강사";
+//			break;
+			
+//		case "자료 구조":
+//			room = "303호";
+//			teacher = "자료강사";
+//			break;
+			
+//		case "linux":
+//			room = "304호";
+//			teacher = "리강사";
+//			break;
+			
+//		case "네트워크 기초":
+//			room = "305호";
+//			teacher = "네강사";
+//			break;
+
+//		default:
+//			System.out.print("\n해당하는 과목이 없습니다.");
+//			room = "해당 강의실 없음";
+//			teacher = "해당 강사 없음";
+//		}
+		
+//		System.out.println("\n과목 : " + subject);
+//		System.out.println("강의실 : " + room);
+//		System.out.println("강사 : " + teacher);
+		
+		
+		
+		
+		
 	}
 
 }
-
-
-
-
-
-
-
-
-
-
-
-

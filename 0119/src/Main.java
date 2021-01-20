9d20cd78b360c0c1e334b6f60805f9d32aecad28
SoYeon, Moon <ansthdus2000@naver.com>
2021-01-21 02:09:05 +0900

    0119

--- 0119/src/Main.java
+++ 0119/src/Main.java
@@ -148,37 +148,127 @@ public class Main {
 		//문제를 3번 틀리면 게임 종료
 		//총점을 출력해 보세요
 		
-		Scanner sc = new Scanner(System.in);
+		//내 풀이
+		//나는 한 구구단 문제에 세 번 이상 틀리면 게임 종료인 줄 알았음
+		//문제 자체를 3문제 이상 틀리면 종료임
+//		Scanner sc = new Scanner(System.in);
 		
-		int first = (int)(Math.random() * 18 + 1);
-		int second = (int)(Math.random() * 8 + 1);
-		int result = first * second;
-		int count = 0;
-		int score = 100;
+//		int first = (int)(Math.random() * 18 + 1);
+//		int second = (int)(Math.random() * 8 + 1);
+		//즉, 이렇게 while문 밖에서 할 필요 없음
+//		int result = first * second;
+//		int count = 0;
+//		int score = 100;
+//		boolean flag = true;
+//		
+//		while(flag || count == 3) {
+//			System.out.print(first + " * " + second + " = ");
+//			int answer = sc.nextInt();
+//			
+//			if(result != answer) {
+//				System.out.println("틀렸습니다. 점수가 5점 감점되었습니다.");
+//				count++;
+//				score = score - 5;
+//			}else if(result == answer) {
+//				System.out.println("정답입니다. 10점을 획득하셨습니다.");
+//				score = score + 10;
+//				
+//				if(first > 10) {
+//					System.out.println("11단 이상의 문제에서 정답을 맞추셨으므로 보너스 점수 10점을 획득하셨습니다.");
+//					score = score + 10;
+//				}
+//				
+//				flag = false;
+//			}
+//		}
+//		
+//		System.out.println("총점 : " + score);
+		
+//		------------------------------------------
+		
+		 //강사님 풀이
+//		 Scanner sc = new Scanner(System.in);
+		 
+		 //  정답                      보너스                    틀림                    총
+//		 int rScore = 10, bScore = 10, wScore = 5, tScore = 0;
+		 //  정답 횟수             틀린 횐수
+//		 int rCount = 0, wCount = 0;
+//		 int answer, result;
+		 
+//		 int fNum, sNum;
+//		 boolean flag = true;
+		 
+//		 while(flag) {
+//			 fNum = (int)(Math.random() * 18 + 2);
+//			 sNum = (int)(Math.random() * 9 + 1);
+//			 result = fNum* sNum;
+			 
+//			 System.out.println("[정답을 입력하세요]");
+//			 System.out.print(fNum + " * " + sNum + " = ");
+//			 answer = sc.nextInt();
+			 
+//			 if(result == answer) {
+//				 System.out.println("정답 입니다." + rScore + "점 획득했습니다.");
+//				 tScore += rScore;
+//				 rCount++;
+				 
+//				 if(fNum >= 11) {
+//					 System.out.println("추가점수" + bScore + "점 획득했습니다.");
+//					 tScore += 10;
+//				 }
+//			 } else {
+//				 System.out.println("틀렸습니다.");
+//				 tScore -= wScore;
+//				 wCount++;
+				 
+//				 if(wCount >= 3) {
+//					 System.out.println(wCount + "번 틀려 게임을 종료합니다.");
+//					 flag = false;
+//				 }
+//			 }
+//		 }
+		 
+//		 System.out.println("정답 횟수 : " + rCount);
+//		 System.out.println("현재 점수 : " + tScore);
+		
+//		------------------------------------------
+		
+//		int num = 0, sum = 0;
+		
+//		do {
+//			System.out.println("while 반복 수행 중입니다.");
+//			num++;
+//			sum = sum + num;
+//		}while(num > 10);
+
+//		System.out.println(sum);
+		
+//		------------------------------------------
+		
+		int num = 0, sum = 0;
+		String eSum = "";
+		
 		boolean flag = true;
-		
-		while(flag || count == 3) {
-			System.out.print(first + " * " + second + " = ");
-			int answer = sc.nextInt();
+		do {
+			num++;
 			
-			if(result != answer) {
-				System.out.println("틀렸습니다. 점수가 5점 감점되었습니다.");
-				count++;
-				score = score - 5;
-			}else if(result == answer) {
-				System.out.println("정답입니다. 10점을 획득하셨습니다.");
-				score = score + 10;
-				
-				if(first > 10) {
-					System.out.println("11단 이상의 문제에서 정답을 맞추셨으므로 보너스 점수 10점을 획득하셨습니다.");
-					score = score + 10;
-				}
-				
-				flag = false;
+			if(num > 10) {
+				break;
 			}
-		}
+			if(num % 2 != 0) {
+				continue;
+			}
+			
+			sum = sum + num;
+			if(num < 10) {
+				eSum += num + " + ";
+			}else {
+				eSum += num + " = ";
+			}
+		}while(flag);
 		
-		System.out.println("총점 : " + score);
+		System.out.println(eSum + sum);
+		
 		
 	}
 
@@ -194,4 +284,8 @@ public class Main {
 
 while
 for
+
+분기 명령
+break    : 현재의 반복 블럭 밖으로 분기
+continue : 현재의 반복 블럭 처음으로 분기
 */
